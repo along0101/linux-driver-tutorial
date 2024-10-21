16640bce86d65ddf806fd31700dc7ebe9cb10562
along <tsilone@qq.com>
2024-10-21 18:02:49 +0800

    [IMP] 添加说明和脚手架

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 ## Reference
 * [编写USB设备驱动程序](https://docs.kernel.org/driver-api/usb/writing_usb_driver.html)
 * [USB Drivers in Linux](https://sysplay.github.io/books/LinuxDrivers/book/Content/Part11.html)
+* [printk日志级别](https://www.kernel.org/doc/html/next/translations/zh_CN/core-api/printk-basics.html)
--- hello/hello.c
+++ hello/hello.c
@@ -6,13 +6,21 @@ MODULE_AUTHOR("zhongqilong");
 MODULE_DESCRIPTION("A simple hello world module");
 
 static int __init hello_init(void){
-    printk(KERN_INFO "Hello, world!\n");
+    // printk(KERN_INFO "Hello, world!\n");
+    printk("Hello, world!\n");
     return 0;
 }
 
 static void __exit hello_exit(void){
-    printk(KERN_INFO "Goodbye, world!\n");
+    // printk(KERN_INFO "Goodbye, world!\n");
+    printk("Goodbye, world!\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+// usage:
+//      make
+//      sudo insmod hello.ko
+//      sudo dmesg | tail
+//      sudo rmmod hello
